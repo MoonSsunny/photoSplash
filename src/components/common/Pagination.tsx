3be2b60b70b66cf725e60e18ad435b22e3096719
MoonSsunny <moonsun116@naver.com>
2023-12-10 16:24:25 +0900

    코드정리

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import colors from 'utils/colors';
 import { PaginationProps } from 'models/photo';
 
